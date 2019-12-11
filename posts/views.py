88dd4cecd8f054621f1cb12e9f3305028c72eaa8
Bianca Ferreira <biancaf2611@gmail.com>
2019-12-10 21:22:00 -0300

    Exercicios

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
+from .forms import PedidoForm
 
 # Create your views here.
 def home(request):
-    nome = 'Bianca'
-    lista = ['Bianca, Caroline, Bruna, Hugo']
-    contexto = {
-        'nome': nome
-    }
-    return render(request, 'home.html', contexto)
+    return render(request, 'home.html')
+
 def post(request):
     return render(request, 'post.html')
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    form = PedidoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        context = {
+            'msg':"Pedido deu certo!"
+        }
+        return render(request, 'cadastro.html', context)
+    context = {
+        'formulario':form 
+    }
+    return render(request, 'cadastro.html', context)
